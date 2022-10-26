b9044beb4cae4f14c7bf1605bbf1b46085f38f9b
Carly Gilson <carly.gilson@softwire.com>
2022-10-26 14:39:12 +0100

    add directories in backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -23,8 +23,22 @@ const getClientDirectory = () => {
     return path.join(currentModuleDirectory, '../../frontend/pages')
 }
 
+const getCssDirectory = () => {
+    const currentModuleFile = fileURLToPath(import.meta.url)
+    const currentModuleDirectory = path.dirname(currentModuleFile)
+    return path.join(currentModuleDirectory, '../../frontend/styles')
+}
+
+const getScriptsDirectory = () => {
+    const currentModuleFile = fileURLToPath(import.meta.url)
+    const currentModuleDirectory = path.dirname(currentModuleFile)
+    return path.join(currentModuleDirectory, '../../frontend/scripts')
+}
+
 // All files in the frontend directory available at "<siteurl>/<filename>"
 app.use('/', express.static(getClientDirectory(), { extensions: ['html'] }))
+app.use('/static/styles/', express.static(getStylesDirectory()))
+app.use('/static/scripts/', express.static(getScriptDirectory()))
 
 // API endpoints available at "<siteurl>/api/<endpoint>"
 app.use('/api', apiRouter)
