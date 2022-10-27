0751321b9cbefd62905ab12bd5beffc5293cf8f8
alexmckenna5 <116561849+alexmckenna5@users.noreply.github.com>
2022-10-27 11:37:21 +0100

    Left and right block movement
    Added left and right movement of blocks

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -3,10 +3,30 @@ const ROWS = 20;
 const BLOCK_SIZE = 40;
 const canvas = document.getElementById("gameCanvas")
 const cxt = canvas.getContext("2d")
+const left_arrow = 37
+const right_arrow = 39
+const down_arrow = 25
 //CONSTANTS
 
 
 let gridMatrix = getEmptygameCanvas(); 
+let blockrow = 0
+let blockcolumn = 0
+
+window.onkeydown = function(move){
+    if (move.keyCode === left_arrow) {
+        blockcolumn--;
+    }
+    else if (move.keyCode === right_arrow) {
+        blockcolumn++;
+    }
+    
+    else if (move.keyCode === down_arrow) {
+        blockrow = blockrow-2;
+    }
+
+    }
+
 
 
 cxt.canvas.width = COLS * BLOCK_SIZE;
