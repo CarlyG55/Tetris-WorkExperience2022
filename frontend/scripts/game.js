f0b6a4301deba8a720eab7d4bc9e2b0c563e0a62
unknown <kamaledine.mehdani@gmail.com>
2022-10-28 10:29:15 +0100

    music

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -1,95 +1,16 @@
-const blocks = [
-    {
-        array: [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
-        ],
-        colour: 'yellow',
-        piece_name: 'oh'
-    },
-       
-    {
-        array: [
-        [0,0,1,0],
-        [0,0,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
-        ],
-        colour: 'blue',
-        piece_name: 'jay'
-    },
-    {
-        array: [
-       [0,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,0,0]
-        ],
-        colour: 'orange',
-        piece_name: 'elle'
-    },
-       
-    {
-        array: [
-        [0,0,0,0],
-        [0,0,1,1],
-        [0,1,1,0],
-        [0,0,0,0]
-        ],
-        colour: 'green',
-        piece_name: 'ess'
-    },
-    {
-        array: [
-        [0,0,0,0],
-        [0,1,1,1],
-        [0,0,1,0],
-        [0,0,0,0]
-        ],
-        colour: 'purple',
-        piece_name: 'tee'
-    },
-    {
-        array: [
-        [0,0,0,0],
-        [1,1,0,0],
-        [0,1,1,0],
-        [0,0,0,0]
-        ],
-        colour: 'red',
-        piece_name: 'zed'
-    },
-       
-    {
-        array: [
-        [0,0,1,0],
-        [0,0,1,0],
-        [0,0,1,0],
-        [0,0,1,0]
-        ],
-        colour: 'cyan',
-        piece_name: 'eye'
-    },
-
-]
-
 const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 40; //
 const canvas = document.getElementById("gameCanvas") 
 const cxt = canvas.getContext("2d")
-const z_key = 90
-const up_key = 38
 const left_arrow = 37
 const right_arrow = 39
-const down_arrow = 40
+const down_arrow = 25
 //CONSTANTS
 
 
 let gridMatrix = getEmptygameCanvas(); 
-let raf = null;
+let timeUntilMoveDown=100;
 let block = generateBlock();
 
 
@@ -99,7 +20,7 @@ cxt.canvas.height = ROWS * BLOCK_SIZE;
 const width = cxt.canvas.width;
 const height = cxt.canvas.height;
 
-//cxt.scale(width, height)
+cxt.scale(width, height)
 
 function getEmptygameCanvas(){
     let matrix = [];
@@ -108,29 +29,11 @@ function getEmptygameCanvas(){
         for (let j=0; j<COLS; j++) {
             matrix[i][j]=0;
             cxt.fillStyle = 0;
-            cxt.fillRect(j*BLOCK_SIZE, i*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            cxt.fillRect(i*BLOCK_SIZE, j*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
         }
     }
     return matrix;
 }
-
-function setBlock(){
-    const blockRow = block.row;
-    const blockColumn = block.column;
-    const blockArray = block.array;
-    const blockColour = block.colour;
-
-    for (let row=0; row<blockArray.length; row++){
-        for (let column=0; column<row.length; column++){
-            const cell = blockArray[row][column];
-            if (cell === 1){
-                gridMatrix[blockRow+row][blockColumn+column] = blockColour;
-            }
-        }
-    }
-    block = generateBlock();
-}
-
 
 //GENERATES GAME CANVAS GRID
 let counter = 0;
@@ -143,12 +46,11 @@ function getEmptyholdCanvas(){
         for (let y=0; y<4; y++) {
             holdMatrix[x][y]=0;
             cxt.fillStyle = 0;
-            cxt.fillRect(y*BLOCK_SIZE, x*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            cxt.fillRect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
         }
     }
     return holdMatrix
 }
-
 
 let nextGridMatrix = getEmptynextCanvas();
 
@@ -159,7 +61,7 @@ function getEmptynextCanvas(){
         for (let y=0; y<4; y++) {
             nextMatrix[x][y]=0;
             cxt.fillStyle = 0;
-            cxt.fillRect(y*BLOCK_SIZE, x*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            cxt.fillRect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
         }
     }
     return nextMatrix
@@ -169,56 +71,39 @@ function generateBlock(){
     let randomNumber = Math.floor(Math.random()*7);
     const randomBlock = blocks[randomNumber]
     return {array: randomBlock.array,
-    colour: randomBlock.colour,
+    color: randomBlock.colour,
     row: -1,
-    column: 5}
+    column: 5}}
+
+function updateTimeUntilMoveDown(){
+    if (timeUntilMoveDown > 15);
+    timeUntilMoveDown = timeUntilMoveDown - 5
 }
-
-function clockwise_rotate(arr){
-   const newarray = arr[0].map((val, index) => arr.map(row => row[index]).reverse());
-   return newarray;
-   }
-    
-function anti_rotate(arr){
-    const newarray = arr[0].map((val, index) => arr.map(row => row[row.length-1-index]));
-    return newarray;
-    }
-        
-window.onkeydown = function(rotate){
-    if (rotate.keyCode === up_key) {
-        block.array = clockwise_rotate(block.array)
-    }
-    else if (rotate.keyCode === z_key) {
-        block.array = anti_rotate(block.array)
-    }
-    }
-
 // create main game function
 function game(){
-    raf = requestAnimationFrame(game);
     // remake game canvas
     cxt.clearRect(0,0,width,height);
     //draw fixed blocks
     for (let i=0; i<ROWS; i++) {
         for (let j=0; j<COLS; j++) {
-            if (gridMatrix[i][j] !== 0) {
-                cxt.fillStyle = gridMatrix[i][j];
-                cxt.fillRect(j*BLOCK_SIZE, i*BLOCK_SIZE, BLOCK_SIZE-0.5, BLOCK_SIZE-0.5)
-            }
+            cxt.fillStyle = gridMatrix[i][j];
+            cxt.fillRect(i*BLOCK_SIZE, j*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
         }
     }
     //move block down
     counter++;
-    if (counter > 100){
+    if (counter > timeUntilMoveDown){
         counter=0;
         block.row++;
     }
+    
+
     //fill block in play
     for (let row=0; row<block.array.length; row++) {
-        for (let col=0; col<block.array[row].length; col++) {
-            if (block.array[row][col] !== 0){
+        for (let col=0; col<row.length; col++) {
+            if (block.array[row][col] !=0){
                 cxt.fillStyle = block.colour;
-                cxt.fillRect((block.column+col)*BLOCK_SIZE, (block.row+row)*BLOCK_SIZE, BLOCK_SIZE-0.5, BLOCK_SIZE-0.5)
+                cxt.fillRect((block.row+row)*BLOCK_SIZE, (block.col+col)*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
             }
         }
     }
@@ -228,17 +113,14 @@ window.onkeydown = function(move){
     if (move.keyCode === left_arrow) {
         block.column--;
     }
-    if (move.keyCode === right_arrow) {
+    else if (move.keyCode === right_arrow) {
         block.column++;
     }
-    if (move.keyCode === down_arrow) {
-        block.row++;
+    
+    else if (move.keyCode === down_arrow) {
+        block.row--;
     }
-    if (move.keyCode === up_key) {
-        block.array = clockwise_rotate(block.array)
-    }
-    if (move.keyCode === z_key) {
-        block.array = anti_rotate(block.array)
-    }
+
 }
-raf = requestAnimationFrame(game);
+
+setInterval(updateTimeUntilMoveDown, 60000)
--- frontend/scripts/gameOver.js
+++
@@ -1,5 +0,0 @@
-
-let score = document.getElementById("thescore").score;
-let user = document.getElementById("thename").user;
-let scores = [user, score]
-writeFile(Path, scores, callback)
